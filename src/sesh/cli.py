b76091a888ba72f6ff09f3f428b1e2f5a8a2af13
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-08-15 10:21:13 +0800

    feat: add tagging support

--- src/sesh/cli.py
+++ src/sesh/cli.py
@@ -5,19 +5,29 @@
 
 import click
 
+from .command_utils.tag import TagOption, Tag
+from .command_utils.start import StartArg, handle_start
+
+
 @click.group()
 def main():
     pass
 
-@main.command()
-def start():
-    click.echo("start sesh")
 
 @main.command()
+@click.option("-t", "--tag", "tags", type=TagOption(), help="Tags to be assigned")
+@click.argument("description", nargs=-1, type=StartArg())
+def start(tags: list[Tag], description: tuple[str | Tag]):
+    """Start a new session"""
+    click.echo("start sesh")
+    handle_start(tags, description)
+
+
+@main.command(help="Stop the current session")
 def stop():
     click.echo("stop sesh")
 
-@main.command()
+
+@main.command(help="Show current session's status")
 def status():
     click.echo("show sesh status")
-
