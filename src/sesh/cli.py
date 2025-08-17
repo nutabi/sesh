53ff2ace42a7a1e97d16a9b693f39a846b7e41bd
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-08-17 20:10:42 +0800

    feat: improve help message

--- src/sesh/cli.py
+++ src/sesh/cli.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 import click
 
 
+from sesh.store import Store
 from sesh.tag import TagOption, Tag
 from sesh.command.start import StartArg, handle_start
 from sesh.command.stop import handle_stop
@@ -8,16 +10,34 @@ from sesh.command.status import handle_status
 
 
 @click.group()
-def main():
+@click.pass_context
+def main(ctx):
+    ctx.obj = Store(Path.cwd() / ".sesh")
     pass
 
 
 @main.command()
-@click.option("-t", "--tag", "tags", type=TagOption(), help="Tags to be assigned")
-@click.argument("description", nargs=-1, type=StartArg())
-def start(tags: list[Tag], description: tuple[str | Tag]):
-    """Start a new Sesh"""
-    handle_start(tags, description)
+@click.option(
+    "-t", "--tag", "tags", type=TagOption(), help="Tags to be assigned", default=[]
+)
+@click.argument("arg", nargs=-1, type=StartArg())
+@click.pass_obj
+def start(store: Store, tags: list[Tag], arg: tuple[str | Tag]):
+    """Start a new Sesh
+
+    ARG is a tag-enabled title. It serves as the title for the new Sesh.
+    However, if a word is prefixed with a plus (e.g. +tag), it will also be
+    treated and included as an inline tag.
+
+    For example, when you run `sesh start do +maths homework`, the title will
+    be "do maths homework" and the tag is "maths".
+    """
+    # check that arg is non-empty
+    if not arg:
+        click.echo("Error: No argument provided.", err=True)
+        return
+
+    handle_start(store, tags, arg)
 
 
 @main.command()
